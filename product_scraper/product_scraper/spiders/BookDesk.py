6048b4e7ed637d8c40f44508e37c99d8fc6964b3
Ruslan Osmanov <rus.osmanov.2021@list.ru>
2022-05-08 19:07:10 +0300

    a

--- product_scraper/product_scraper/spiders/BookDesk.py
+++ product_scraper/product_scraper/spiders/BookDesk.py
@@ -5,16 +5,16 @@ class BookCheck(scrapy.Spider):
     start_urls = ["https://avidreaders.ru/books/"]
 
     def parseLink(self, response):
-        for products in response.css("div.wrap_description"):
-            yield {
-                "description": products.css("p").get()
-            }
-
-
+        global desc
+        desc = response.css("div.wrap_description p").get()
 
     def parse(self, response):
         for products in response.css("div.card_info"):
+            self.parseLink(response)
             yield {
-                "name" : products.css("div.book_name a::text").get()
+                "name": products.css("div.book_name a::text").get(),
+                "description": desc
             }
-        next_page = response.css("div.current").attrib["href"]
+        next_page = response.css("div.pagination li:last-child").attrib["href"]
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
