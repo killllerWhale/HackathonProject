4b7914bcae235333a614200221e839f85ba01ee0
Ruslan Osmanov <rus.osmanov.2021@list.ru>
2022-05-08 18:39:56 +0300

    F

--- product_scraper/product_scraper/spiders/BookDesk.py
+++ product_scraper/product_scraper/spiders/BookDesk.py
@@ -4,6 +4,14 @@ class BookCheck(scrapy.Spider):
     name = "books"
     start_urls = ["https://avidreaders.ru/books/"]
 
+    def parseLink(self, response):
+        for products in response.css("div.wrap_description"):
+            yield {
+                "description": products.css("p").get()
+            }
+
+
+
     def parse(self, response):
         for products in response.css("div.card_info"):
             yield {
