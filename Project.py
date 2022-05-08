e8b53da71c62084b4957fd42faefe8fd525b797c
killllerWhale <93337303+killllerWhale@users.noreply.github.com>
2022-05-08 23:23:17 +0300

    designer

--- Project.py
+++ Project.py
@@ -1,3 +1,21 @@
-print("Слава")
-print("Heckfy")
-print("hello")
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QMessageBox
+from mydesign import Ui_MainWindow
+
+
+class Book(QMainWindow):
+    def __init__(self):
+        super(Book, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.start()
+        self.checkin_completion = 0
+
+    def start(self):
+        pass
+
+if __name__ == '__main__':
+    app = QApplication([])
+    application = Book()
+    application.show()
+    sys.exit(app.exec())
