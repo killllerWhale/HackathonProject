71d4eec75914e4912487b2007440406b3899a62f
Ruslan Osmanov <rus.osmanov.2021@list.ru>
2022-05-08 23:13:58 +0300

    Ееееее

--- product_scraper/product_scraper/items.py
+++ product_scraper/product_scraper/items.py
@@ -8,5 +8,5 @@ import scrapy
 
 class ProductScraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    description = scrapy.Field()
